1afe0ea4be421060230d03f6859f480cb76654ff
Gabriel Rodrigo <93385461+Gabriel-GR1@users.noreply.github.com>
2026-09-14 12:36:24 -0300

    Update WhatsApp group link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WHATSAPP_GROUP_URL = 'https://chat.whatsapp.com/GCi2WhyhAPXEExFhNszgoX?s=cl&p=a&mlu=4&ilr=4';
+const WHATSAPP_GROUP_URL = 'https://chat.whatsapp.com/EdzyVNDL1B4FIjU8irkXNP';
 
 const menuButton = document.querySelector('.menu-button');
 const menu = document.querySelector('.menu');
